b1667315e225b66bc2074d3ae8f28733ad99d284
Flopster101 <nahuelgomez329@gmail.com>
2025-04-01 18:55:05 -0300

    include: binfmts: extend task_is_booster cases
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -164,7 +164,12 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@2.2-servic") ||
 	       !strcmp(comm, "power@2.0-servic") ||
 	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	       !strcmp(comm, "perf2-hal-servi") ||
+	       !strcmp(comm, "init.qcom.post_") ||
+	       !strcmp(comm, "PERFD-SERVER") ||
+	       !strcmp(comm, "mtkPowerMsgHdl") ||
+	       !strcmp(comm, "mtkPowerService") ||
+	       !strncmp(comm, "system_perf_ini", 9);
 }
 
 #endif /* _LINUX_BINFMTS_H */
