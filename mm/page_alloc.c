6c9149e3c06917bf57a10c6b44da34b736f81854
Richard Raya <rdxzv.dev@gmail.com>
2026-06-20 16:02:24 -0300

    mm/page_alloc: Fix DDR/CPU boost placing
    Change-Id: Ie96dece42c7d5faa269fccc7ce3829719fae05fc
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4263,11 +4263,6 @@ retry:
 	if (gfp_mask & __GFP_NORETRY)
 		goto nopage;
 
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(150);
-	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 150);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
-
 	/*
 	 * Do not retry costly high order allocations unless they are
 	 * __GFP_RETRY_MAYFAIL and we can compact
